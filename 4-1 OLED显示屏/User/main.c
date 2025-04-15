ab796fb29fd8d03ebba35b34eb8afa1378da22c8
qazwsx2323ppp <2220761584@qq.com>
2025-04-15 10:55:20 +0800

    4.15

--- 4-1 OLED显示屏/User/main.c
+++ 4-1 OLED显示屏/User/main.c
@@ -1,14 +1,23 @@
 #include "stm32f10x.h"                  // Device header
 #include "Delay.h"
-
+#include "OLED.h"
 
 uint8_t KeyNum;
 
 int main(void)
 {
-	   
-    while (1)
-   {
+    OLED_Init();
+	
+	OLED_ShowChar(1, 1, 'A');
+	OLED_ShowString(1, 1, "HelloWorld");
+	OLED_ShowNum(2, 1, 12345, 5);
+	OLED_ShowSignedNum(2, 7, -66, 2); //显示带符号数，符号不算入字长
+	OLED_ShowHexNum(3, 1, 0xAA55, 4);
+	
+	OLED_Clear(); //清屏
+	//若想清楚某位置的字符，可使用OLED_ShowString(x, x, "空格");
+       while (1)
+     {
 	  
-   }
+     }
 }
